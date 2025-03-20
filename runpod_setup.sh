0ab2707b834574138a272c4a4bb282597212bf1e
CGA <cgadityan@gmail.com>
2025-03-20 17:30:50 +0530

    bash 1 again

--- runpod_setup.sh
+++ runpod_setup.sh
@@ -4,30 +4,12 @@ set -e
 # Change to the workspace directory (assumed to be mounted from the persistent volume)
 # cd /workspace'
 # Assume xdit_runpod directory exists
-
 pwd
 cd ../workspace/xdit_runpod
 pwd
 
-python -m venv flux
-source flux/bin/activate
-pip install -r requirements.txt
-
-pip install torch==2.4.0 torchvision==0.19.0  --index-url https://download.pytorch.org/whl/cu118
-
-# --- Install Miniconda on the persistent volume if not already installed ---
-# if [ ! -d "/workspace/miniconda3" ]; then
-#     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-#     bash Miniconda3-latest-Linux-x86_64.sh -b -p /workspace/miniconda3
-# fi
-
 # Add the persistent Miniconda to PATH
-# export PATH="/workspace/miniconda3/bin:$PATH"
-
-# --- Setup Conda Environment ---
-# conda env create -f requirements.yml -n flux
-
-pip install "xfuser[diffusers,flash-attn]"
+export PATH="/workspace/miniconda3/bin:$PATH"
 
 # --- Check GPU availability ---
 python gpu_check.py
