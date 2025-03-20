6d506549e37ebe356cb54c874f964a8e6c41aaf0
CGA <cgadityan@gmail.com>
2025-03-20 20:15:53 +0530

    bash 1 again

--- runpod_setup.sh
+++ runpod_setup.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 set -e
 
-# Change to the workspace directory (assumed to be mounted from the persistent volume)
-# cd /workspace'
-# Assume xdit_runpod directory exists
-pwd
-cd ../workspace/xdit_runpod
-pwd
+export PATH="/workspace/miniconda3/bin:$PATH" 
+source /workspace/miniconda3/etc/profile.d/conda.sh
+source ~/.bashrc
+conda init --all
+# """ps aux | grep "sshd: root" | grep -v grep | awk '{print $2}' | xargs kill -9""",
 
-# Add the persistent Miniconda to PATH
-export PATH="/workspace/miniconda3/bin:$PATH"
-
-# --- Check GPU availability ---
-python gpu_check.py
-
-# --- Login to Hugging Face ---
-huggingface-cli login --token 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
-
-# --- Run Inference using torchrun ---
-torchrun --nproc_per_node=2 ./xdit_usp.py --model black-forest-labs/FLUX.1-Fill-dev --ulysses_degree 2 --ring_degree 1 --num_inference_steps 50 --size 1224,1664
-
-exit 
+conda activate flux
+python /workspace/xdit_runpod/gpu_check.py 
+huggingface-cli login --token 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 
+torchrun --nproc_per_node=2 /workspace/xdit_runpod/xdit_usp.py --model black-forest-labs/FLUX.1-Fill-dev --ulysses_degree 2 --ring_degree 1 --num_inference_steps 50 --size 1224,1664
