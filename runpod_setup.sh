64cd6147232dd59e299c188382a0223bddc11741
CGA <cgadityan@gmail.com>
2025-03-20 14:42:47 +0530

    bash 1 and 2

--- runpod_setup.sh
+++ runpod_setup.sh
@@ -2,11 +2,8 @@
 set -e
 
 # Change to the workspace directory (assumed to be mounted from the persistent volume)
-# cd /workspace
-
-# Create directories if they do not exist
-mkdir -p cgadityan
-
+# cd /workspace'
+# Assume xdit_runpod directory exists
 
 # --- Install Miniconda on the persistent volume if not already installed ---
 if [ ! -d "/workspace/miniconda3" ]; then
@@ -17,28 +14,9 @@ fi
 # Add the persistent Miniconda to PATH
 export PATH="/workspace/miniconda3/bin:$PATH"
 
-cd cgadityan
-
-# --- Clone the xDiT Repository if not already present ---
-if [ ! -d "xDiT" ]; then
-    git clone https://github.com/cgadityan/xdit_runpod.git
-fi
-
+cd xdit_runpod
 
 # --- Setup Conda Environment ---
 conda env create -f requirements.yml -n flux
-conda deactivate
-conda activate flux
 
-# --- Install additional dependencies ---
-pip install "xfuser[diffusers,flash-attn]"
-
-# --- Check GPU availability ---
-python gpu_check.py
-
-# --- Login to Hugging Face ---
-huggingface-cli login --token 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
-
-# --- Run Inference using torchrun ---
-torchrun --nproc_per_node=2 ./xdit_usp.py --model black-forest-labs/FLUX.1-Fill-dev --ulysses_degree 2 --ring_degree 1 --num_inference_steps 50 --size 1224,1664
-
+exit 
